69d40b6ba15f6036108692d11b287b6de7a007cb
jratcliff <jratcliff@nvidia.com>
2018-07-10 16:10:57 -0600

    More refactor

--- ext/easywsclient/easywsclient.cpp
+++ ext/easywsclient/easywsclient.cpp
@@ -55,9 +55,134 @@ namespace easywsclient
 			uint8_t		masking_key[4];
 		};
 
-		WebSocketImpl(wsocket::Wsocket *_sockfd, bool useMask) : mSocket(_sockfd), mReadyState(OPEN), mUseMask(useMask)
+		WebSocketImpl(const char *url,const char *origin, bool useMask) : mReadyState(OPEN), mUseMask(useMask)
 		{
 			mTransmitBuffer = simplebuffer::SimpleBuffer::create(DEFAULT_TRANSMIT_BUFFER_SIZE);
+			size_t urlSize = strlen(url);
+			size_t originSize = strlen(origin);
+			char host[128];
+			int port=0;
+			char path[128];
+			if (urlSize >= 128)
+			{
+				fprintf(stderr, "ERROR: url size limit exceeded: %s\n", url);
+			}
+			else if (originSize >= 200)
+			{
+				fprintf(stderr, "ERROR: origin size limit exceeded: %s\n", origin);
+			}
+			else
+			{
+				if (sscanf(url, "ws://%[^:/]:%d/%s", host, &port, path) == 3)
+				{
+				}
+				else if (sscanf(url, "ws://%[^:/]/%s", host, path) == 2)
+				{
+					port = 80;
+				}
+				else if (sscanf(url, "ws://%[^:/]:%d", host, &port) == 2)
+				{
+					path[0] = '\0';
+				}
+				else if (sscanf(url, "ws://%[^:/]", host) == 1)
+				{
+					port = 80;
+					path[0] = '\0';
+				}
+				else
+				{
+					fprintf(stderr, "ERROR: Could not parse WebSocket url: %s\n", url);
+				}
+				if (port)
+				{
+					fprintf(stderr, "easywsclient: connecting: host=%s port=%d path=/%s\n", host, port, path);
+					mSocket = wsocket::Wsocket::create(host, port);
+					if (mSocket == nullptr)
+					{
+						fprintf(stderr, "Unable to connect to %s:%d\n", host, port);
+					}
+					else
+					{
+						// XXX: this should be done non-blocking,
+						char line[256];
+						int status;
+						int i;
+						wplatform::stringFormat(line, 256, "GET /%s HTTP/1.1\r\n", path);
+						mSocket->send(line, uint32_t(strlen(line)));
+						if (port == 80)
+						{
+							wplatform::stringFormat(line, 256, "Host: %s\r\n", host);
+							mSocket->send(line, uint32_t(strlen(line)));
+						}
+						else
+						{
+							wplatform::stringFormat(line, 256, "Host: %s:%d\r\n", host, port);
+							mSocket->send(line, uint32_t(strlen(line)));
+						}
+						wplatform::stringFormat(line, 256, "Upgrade: websocket\r\n");
+						mSocket->send(line, uint32_t(strlen(line)));
+						wplatform::stringFormat(line, 256, "Connection: Upgrade\r\n");
+						mSocket->send(line, uint32_t(strlen(line)));
+						if (originSize)
+						{
+							wplatform::stringFormat(line, 256, "Origin: %s\r\n", origin);
+							mSocket->send(line, uint32_t(strlen(line)));
+						}
+						wplatform::stringFormat(line, 256, "Sec-WebSocket-Key: x3JJHMbDL1EzLkh9GBhXDw==\r\n");
+						mSocket->send(line, uint32_t(strlen(line)));
+						wplatform::stringFormat(line, 256, "Sec-WebSocket-Version: 13\r\n");
+						mSocket->send(line, uint32_t(strlen(line)));
+						wplatform::stringFormat(line, 256, "\r\n");
+						mSocket->send(line, uint32_t(strlen(line)));
+						for (i = 0; i < 2 || (i < 255 && line[i - 2] != '\r' && line[i - 1] != '\n'); ++i)
+						{
+							if (mSocket->receive(line + i, 1) == 0)
+							{
+								mSocket->release();
+								mSocket = nullptr;
+								break;
+							}
+						}
+						line[i] = 0;
+						if (i == 255)
+						{
+							fprintf(stderr, "ERROR: Got invalid status line connecting to: %s\n", url);
+							mSocket->release();
+							mSocket = nullptr;
+						}
+						if (sscanf(line, "HTTP/1.1 %d", &status) != 1 || status != 101)
+						{
+							fprintf(stderr, "ERROR: Got bad status connecting to %s: %s", url, line);
+							mSocket->release();
+							mSocket = nullptr;
+						}
+						if (mSocket)
+						{
+							// TODO: verify response headers,
+							while (true)
+							{
+								for (i = 0; i < 2 || (i < 255 && line[i - 2] != '\r' && line[i - 1] != '\n'); ++i)
+								{
+									if (mSocket->receive(line + i, 1) == 0)
+									{
+										mSocket->release();
+										mSocket = nullptr;
+										break;
+									}
+								}
+								if (line[0] == '\r' && line[1] == '\n')
+								{
+									break;
+								}
+							}
+						}
+					}
+					if (mSocket)
+					{
+						mSocket->disableNaglesAlgorithm();
+					}
+				}
+			}
 		}
 
 		virtual ~WebSocketImpl(void)
@@ -407,6 +532,12 @@ namespace easywsclient
 			uint8_t closeFrame[6] = { 0x88, 0x80, 0x00, 0x00, 0x00, 0x00 }; // last 4 bytes are a masking key
 			mTransmitBuffer->addBuffer(closeFrame, sizeof(closeFrame));
 		}
+
+		bool isValid(void) const
+		{
+			return mSocket ? true : false;
+		}
+
 	private:
 		std::vector<uint8_t>		mReceiveBuffer;			// receive buffer
 		simplebuffer::SimpleBuffer *mTransmitBuffer{ nullptr };			// transmit buffer
@@ -416,124 +547,14 @@ namespace easywsclient
 		bool						mUseMask{ true };
 };
 
-WebSocket *WebSocket::create(const char *_url, const char *_origin,bool useMask)
+WebSocket *WebSocket::create(const char *url, const char *origin,bool useMask)
 {
-	std::string url(_url);
-	std::string origin(_origin);
-
-	char host[128];
-	int port;
-	char path[128];
-	if (url.size() >= 128)
+	auto ret = new WebSocketImpl(url, origin, useMask);
+	if (!ret->isValid())
 	{
-		fprintf(stderr, "ERROR: url size limit exceeded: %s\n", url.c_str());
-		return NULL;
+		ret->release();
+		ret = nullptr;
 	}
-	if (origin.size() >= 200)
-	{
-		fprintf(stderr, "ERROR: origin size limit exceeded: %s\n", origin.c_str());
-		return NULL;
-	}
-	if (false)
-	{
-	}
-	else if (sscanf(url.c_str(), "ws://%[^:/]:%d/%s", host, &port, path) == 3)
-	{
-	}
-	else if (sscanf(url.c_str(), "ws://%[^:/]/%s", host, path) == 2)
-	{
-		port = 80;
-	}
-	else if (sscanf(url.c_str(), "ws://%[^:/]:%d", host, &port) == 2)
-	{
-		path[0] = '\0';
-	}
-	else if (sscanf(url.c_str(), "ws://%[^:/]", host) == 1)
-	{
-		port = 80;
-		path[0] = '\0';
-	}
-	else
-	{
-		fprintf(stderr, "ERROR: Could not parse WebSocket url: %s\n", url.c_str());
-		return NULL;
-	}
-	//fprintf(stderr, "easywsclient: connecting: host=%s port=%d path=/%s\n", host, port, path);
-	wsocket::Wsocket *sockfd = wsocket::Wsocket::create(host, port);
-	if (sockfd == nullptr)
-	{
-		fprintf(stderr, "Unable to connect to %s:%d\n", host, port);
-		return NULL;
-	}
-	{
-		// XXX: this should be done non-blocking,
-		char line[256];
-		int status;
-		int i;
-		wplatform::stringFormat(line, 256, "GET /%s HTTP/1.1\r\n", path);
-		sockfd->send(line, uint32_t(strlen(line)));
-		if (port == 80)
-		{
-			wplatform::stringFormat(line, 256, "Host: %s\r\n", host);
-			sockfd->send(line, uint32_t(strlen(line)));
-		}
-		else
-		{
-			wplatform::stringFormat(line, 256, "Host: %s:%d\r\n", host, port);
-			sockfd->send(line, uint32_t(strlen(line)));
-		}
-		wplatform::stringFormat(line, 256, "Upgrade: websocket\r\n");
-		sockfd->send(line, uint32_t(strlen(line)));
-		wplatform::stringFormat(line, 256, "Connection: Upgrade\r\n");
-		sockfd->send(line, uint32_t(strlen(line)));
-		if (!origin.empty())
-		{
-			wplatform::stringFormat(line, 256, "Origin: %s\r\n", origin.c_str());
-			sockfd->send(line, uint32_t(strlen(line)));
-		}
-		wplatform::stringFormat(line, 256, "Sec-WebSocket-Key: x3JJHMbDL1EzLkh9GBhXDw==\r\n");
-		sockfd->send(line, uint32_t(strlen(line)));
-		wplatform::stringFormat(line, 256, "Sec-WebSocket-Version: 13\r\n");
-		sockfd->send(line, uint32_t(strlen(line)));
-		wplatform::stringFormat(line, 256, "\r\n");
-		sockfd->send(line, uint32_t(strlen(line)));
-		for (i = 0; i < 2 || (i < 255 && line[i - 2] != '\r' && line[i - 1] != '\n'); ++i)
-		{
-			if (sockfd->receive(line + i, 1) == 0)
-			{
-				return NULL;
-			}
-		}
-		line[i] = 0;
-		if (i == 255)
-		{
-			fprintf(stderr, "ERROR: Got invalid status line connecting to: %s\n", url.c_str());
-			return NULL;
-		}
-		if (sscanf(line, "HTTP/1.1 %d", &status) != 1 || status != 101)
-		{
-			fprintf(stderr, "ERROR: Got bad status connecting to %s: %s", url.c_str(), line);
-			return NULL;
-		}
-		// TODO: verify response headers,
-		while (true)
-		{
-			for (i = 0; i < 2 || (i < 255 && line[i - 2] != '\r' && line[i - 1] != '\n'); ++i)
-			{
-				if (sockfd->receive(line + i, 1) == 0)
-				{
-					return NULL;
-				}
-			}
-			if (line[0] == '\r' && line[1] == '\n')
-			{
-				break;
-			}
-		}
-	}
-	sockfd->disableNaglesAlgorithm();
-
-	auto ret = new WebSocketImpl(sockfd, useMask);
 	return static_cast<WebSocket *>(ret);
 }
 
